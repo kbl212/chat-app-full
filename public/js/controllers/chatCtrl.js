6eb18b174b80844656b9ce83a778cdddec330756
kbl212 <kyle.lauritzen@gmail.com>
2016-03-12 21:44:20 -0700

    put messages in an array...messages still  not showing up instantly.

--- public/js/controllers/chatCtrl.js
+++ public/js/controllers/chatCtrl.js
@@ -2,19 +2,17 @@ var app = angular.module('chat-app-full');
 
 app.controller('chatCtrl', function($scope, chatService){
     $scope.currMessage = "";
-    $scope.fullChat = "";
+    $scope.allChatMessages = [];
     $scope.socket = io.connect();
     
     $scope.messageSubmit = function() {
-        alert('$scope.currMessage = ', $scope.currMessage);
         console.log($scope.currMessage);
         $scope.socket.emit('send message', $scope.currMessage);
         $scope.currMessage = "";
     }
     
     $scope.socket.on('new message', function(data) {
-        console.log("this...is...DATA!: ", data);
-        $scope.fullChat += data + "\n";
-        console.log("what", $scope.fullChat);
+        $scope.allChatMessages.push(data);
+        console.log("FullChat:  ", $scope.allChatMessages);
     });
 })
