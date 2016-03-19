ef8c6ab1d5a057f6fce094407f306cfa86cb0198
kbl212 <kyle.lauritzen@gmail.com>
2016-03-19 03:23:57 -0600

    chat functional between several users...displays userName and timestamp correctly as well.

--- public/js/controllers/chatCtrl.js
+++ public/js/controllers/chatCtrl.js
@@ -4,16 +4,45 @@ app.controller('chatCtrl', function($scope, chatService){
     $scope.currMessage = "";
     $scope.allChatMessages = [];
     $scope.socket = io.connect();
+    $scope.userName = "Anonymous";
    // $scope.fullChatDiv = "";
     
     $scope.messageSubmit = function() {
         console.log($scope.currMessage);
-        $scope.socket.emit('send message', $scope.currMessage);
+        var submitObj = {
+            message: $scope.currMessage,
+            userName: $scope.userName
+        }
+        $scope.socket.emit('send message', submitObj);
         $scope.currMessage = "";
     }
     
     $scope.socket.on('new message', function(data) {
-        $scope.allChatMessages.push(data);
+       
+        var today = new Date();
+        var amPM = "AM";
+        var hours = today.getHours();
+        var minutes = today.getMinutes();
+        var seconds = today.getSeconds();
+        if (hours > 11) {
+            hours -= 12;
+            amPM = "PM";
+        }
+        if (hours < 10)
+            hours = "0" + hours;
+        if (minutes < 10)
+            minutes = "0" + minutes;
+        if (seconds < 10)
+            seconds = "0" + seconds;
+
+        var currTime = hours + ":" + minutes + ":" + seconds + " " + amPM;
+        
+        var messageObj = {
+            userName: data.userName,
+            content: data.message,
+            time: currTime
+        }
+        $scope.allChatMessages.push(messageObj);
         $scope.$apply();
         console.log("FullChat:  ", $scope.allChatMessages);
     });
